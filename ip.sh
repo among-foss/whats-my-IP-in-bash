5a7d8d66978856334de323340d66536701ef61b7
among-foss <205830254+among-foss@users.noreply.github.com>
2025-04-07 14:01:49 +0000

    Update ip.sh
    - Add https://ip.hostux.net

--- ip.sh
+++ ip.sh
@@ -7,25 +7,36 @@ response=$(curl --silent "$1")
     if [ $? -eq 0 ]; then
         echo "Your public ip is: "
         echo "$response"
+
     else
         response=$(curl --silent "$2")
 
         if [ $? -eq 0 ]; then
-            echo "Your ip is: "
+            echo "Your public ip is: "
             echo "$response"
+
         else
-            response=$(curl --silent "$3" | grep -oP '(?<=<data>).*?(?=</data>)')
+            response=$(curl --silent "$3")
 
             if [ $? -eq 0 ]; then
-                echo "Your ip is: "
+                echo "Your public ip is: "
                 echo "$response"
-            else
-                echo "Failed to connect to $1, $2 and $3"
 
+            else
+                response=$(curl --silent "$4" | grep -oP '(?<=<data>).*?(?=</data>)')
+
+                if [ $? -eq 0 ]; then
+                    echo "Your public ip is: "
+                    echo "$response"
+
+                else
+                    echo "Failed to connect to $1, $2, $3 and $4"
+
+                fi
             fi
         fi
     fi
 
 }
 
-get_public_ip "https://ipv4.myip.coffee" "https://4.ipwho.de/ip" "https://artemislena.eu/services/whoareyou.html"
+get_public_ip "https://ipv4.myip.coffee" "https://4.ipwho.de/ip" "https://ip.hostux.net" "https://artemislena.eu/services/whoareyou.html"
